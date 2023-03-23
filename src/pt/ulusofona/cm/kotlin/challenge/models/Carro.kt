5f4cf3055b7b2d37d8fdaede87b648014760fbd3
Filipe <a21905158@alunos.ulht.pt>
2023-03-23 00:30:35 +0000

    Third commit.

--- src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
@@ -1,10 +1,21 @@
 package pt.ulusofona.cm.kotlin.challenge.models
+import pt.ulusofona.cm.kotlin.challenge.exceptions.VeiculoDesligadoException
 import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
 
 class Carro(identificador: String, var motor: Motor) : Veiculo(identificador) {
     override fun requerCarta(): Boolean {
         return true
     }
+
+    override fun moverPara(x: Int, y: Int) {
+        if(motor.estaLigado()){
+            super.moverPara(x, y)
+            return
+        }
+
+        throw VeiculoDesligadoException()
+    }
+
 
     override fun toString(): String {
         return "Carro | ${super.toString()}"
