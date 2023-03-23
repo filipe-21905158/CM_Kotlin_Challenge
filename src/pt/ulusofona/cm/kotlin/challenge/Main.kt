8c422cc0a7affaf41e64fa42815ff62edc20f5b2
Filipe <a21905158@alunos.ulht.pt>
2023-03-23 23:57:17 +0000

    Third commit.

--- src/pt/ulusofona/cm/kotlin/challenge/Main.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/Main.kt
@@ -1,11 +1,26 @@
 package pt.ulusofona.cm.kotlin.challenge
 
+import pt.ulusofona.cm.kotlin.challenge.models.Carro
+import pt.ulusofona.cm.kotlin.challenge.models.Carta
+import pt.ulusofona.cm.kotlin.challenge.models.Motor
+import pt.ulusofona.cm.kotlin.challenge.models.Pessoa
+import java.time.LocalDate
+import java.time.Period
+import java.util.Date
+
+
 fun main() {
     // aqui escreves o código do programa
 
     /*var newCar = Carro("12345", Motor(230, 2000))
     println(newCar.motor.estaLigado())
-    println(newCar.motor.desligar())
+    println(newCar.motor.ligar())
+    println(newCar.motor.ligar())
     println(newCar.motor.estaLigado())*/
+
+    /*val dataAtual =  LocalDate.now()
+    //val dataNascimento = LocalDate.of(dataDeNascimento._ano, dataDeNascimento._mes, dataDeNascimento._dia)
+    val dataNascimento = LocalDate.of(2003, 3, 23)
+    println(Period.between(dataNascimento, dataAtual).years)*/
 }
 
--- src/pt/ulusofona/cm/kotlin/challenge/models/Pessoa.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Pessoa.kt
@@ -58,7 +58,7 @@ class Pessoa(
     fun tirarCarta() {
         val dataAtual =  LocalDate.now()
         //val dataNascimento = LocalDate.of(dataDeNascimento._ano, dataDeNascimento._mes, dataDeNascimento._dia)
-        val dataNascimento = LocalDate.of(dataDeNascimento.year, dataDeNascimento.month + 1, dataDeNascimento.day)
+        val dataNascimento = LocalDate.of(dataDeNascimento.year, dataDeNascimento.month, dataDeNascimento.day)
 
 
         if (Period.between(dataNascimento, dataAtual).years >= 18) {
