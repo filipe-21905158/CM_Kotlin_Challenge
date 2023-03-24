20d34b9c1f1f90a9be3b2cdba8b7e77c813dab79
Filipe <a21905158@alunos.ulht.pt>
2023-03-24 01:06:08 +0000

    Third commit.

--- src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Carro.kt
@@ -7,8 +7,9 @@ class Carro(identificador: String, var motor: Motor) : Veiculo(identificador) {
     }
 
     override fun moverPara(x: Int, y: Int) {
-
+        if(motor.estaLigado()){
             super.moverPara(x, y)
+        }
     }
 
 
